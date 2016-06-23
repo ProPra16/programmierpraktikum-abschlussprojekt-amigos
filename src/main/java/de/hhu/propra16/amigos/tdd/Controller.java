0eb0c206feee83530095b1e708ab58b1cd17948f
Josias <jouhl101@hhu.de>
2016-06-23 23:22:31 +0200

    placeholder project

---
+++ src/main/java/de/hhu/propra16/amigos/tdd/Controller.java
@@ -0,0 +1,4 @@
+package de.hhu.propra16.amigos.tdd;
+
+public class Controller {
+}
---
+++ src/test/java/de/hhu/propra16/amigos/tdd/tests/SampleTest.java
@@ -0,0 +1,8 @@
+package de.hhu.propra16.amigos.tdd.tests;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+public class SampleTest {
+
+}
