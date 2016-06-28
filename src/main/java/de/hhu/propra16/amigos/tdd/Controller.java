85eb7d6a7239d63deb1350a310f071b835e5927c
Josias <jouhl101@hhu.de>
2016-06-28 03:28:14 +0200

    Add rsyntaxarea experiment

--- src/main/java/de/hhu/propra16/amigos/tdd/Controller.java
+++ src/main/java/de/hhu/propra16/amigos/tdd/Controller.java
@@ -1,4 +1,61 @@
 package de.hhu.propra16.amigos.tdd;
 
-public class Controller {
+import javafx.embed.swing.SwingNode;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import org.fife.rsta.ac.LanguageSupportFactory;
+import org.fife.ui.rsyntaxtextarea.RSyntaxTextArea;
+import org.fife.ui.rsyntaxtextarea.SyntaxConstants;
+import org.fife.ui.rtextarea.RTextScrollPane;
+
+import javax.swing.*;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class Controller implements Initializable{
+
+    @FXML
+    public SwingNode swingNode;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                RSyntaxTextArea textArea = new RSyntaxTextArea(20, 60);
+                LanguageSupportFactory.get().register(textArea);
+                textArea.setSyntaxEditingStyle(SyntaxConstants.SYNTAX_STYLE_JAVA);
+                textArea.setCodeFoldingEnabled(true);
+                RTextScrollPane sp = new RTextScrollPane(textArea);
+                swingNode.setContent(sp);
+                textArea.setText(String.join("\n", new String[] {
+                        "package com.example;",
+                        "",
+                        "import java.util.*;",
+                        "",
+                        "public class Foo extends Bar implements Baz {",
+                        "",
+                        "    /*",
+                        "     * Vorteile:",
+                        "     * -automatische Einrückung",
+                        "     * -setzt automatisch Klammern",
+                        "     * -Code completion per Ctrl-Space",
+                        "     * Nachteile:",
+                        "     * -ist per Swing eingebettet, könnte instabil werden",
+                        "     */",
+                        "    public static void main(String[] args) {",
+                        "        // single-line comment",
+                        "        for(String arg: args) {",
+                        "            if(arg.length() != 0)",
+                        "                System.out.println(arg);",
+                        "            else",
+                        "                System.err.println(\"Warning: empty string as argument\");",
+                        "        }",
+                        "    }",
+                        "",
+                        "}"
+                }));
+            }
+        });
+    }
 }
