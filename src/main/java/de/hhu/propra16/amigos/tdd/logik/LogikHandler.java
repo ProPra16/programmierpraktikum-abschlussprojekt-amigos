a58fa6a0cc6e8d1f52e5356e6788d3bd0028241c
Oliver Iusztin <oliver.iusztin@uni-duesseldorf.de>
2016-07-02 18:07:39 +0200

    test

--- src/main/java/de/hhu/propra16/amigos/tdd/logik/LogikHandler.java
+++ src/main/java/de/hhu/propra16/amigos/tdd/logik/LogikHandler.java
@@ -2,5 +2,6 @@ package de.hhu.propra16.amigos.tdd.logik;
 
 
 public class LogikHandler {
-
+    boolean areTestsPassing() { return true; } // true wenn die Tests erfolgreich sind
+    boolean isCodeCompiling() { return true; } // true wenn der Code compiled
 }
