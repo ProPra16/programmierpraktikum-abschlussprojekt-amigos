3c84e8c886fa39c5c87ccd76c456552ded7558ef
Oliver Iusztin <oliver.iusztin@uni-duesseldorf.de>
2016-07-12 19:20:55 +0200

    Fixed several bugs

--- src/test/java/de/hhu/propra16/amigos/tdd/tests/LogikHandlerTestsATDD.java
+++ src/test/java/de/hhu/propra16/amigos/tdd/tests/LogikHandlerTestsATDD.java
@@ -78,12 +78,17 @@ public class LogikHandlerTestsATDD {
     @Test
     public void switchState_MAKE_PASS_TEST_to_WRITE_FAILING_TEST_pass() {
         simpleHandler.setATDDTest(failingATDDTest);
+        simpleHandler.switchState(TDDState.WRITE_FAILING_TEST);
+        simpleHandler.setTest(oneFailingTest);
         simpleHandler.switchState(TDDState.MAKE_PASS_TEST);
         assertTrue(simpleHandler.switchState(TDDState.WRITE_FAILING_TEST));
     }
     @Test
     public void switchState_MAKE_PASS_TEST_to_REFACTOR_fails() {
-     //   simpleHandler.setATDDTest(failingATDDTest);
-     //   assertTrue(simpleHandler.switchState(TDDState.WRITE_FAILING_TEST));
+        simpleHandler.setATDDTest(failingATDDTest);
+        simpleHandler.switchState(TDDState.WRITE_FAILING_TEST);
+        simpleHandler.setTest(oneFailingTest);
+        simpleHandler.switchState(TDDState.MAKE_PASS_TEST);
+        assertFalse(simpleHandler.switchState(TDDState.REFACTOR));
     }
 }
